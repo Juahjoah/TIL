2e575675f11c3192aa5af2294244fce68030d06d
김주아 <07juahlily21@naver.com>
2023-03-31 15:07:52 +0900

    nomad_js test

---
+++ JS/Nomad_js/app.js
@@ -0,0 +1,15 @@
+
+// 2_변수
+console.log(54545454);
+console.log("hahahah");
+
+const a = 5; // 변수선언 함수
+const b = 2; // '2'는 불가능
+console.log(5+2);
+console.log(a+b);
+console.log(5*2);
+console.log(a*b);
+console.log(5/2);
+
+const myName = "jjjjj";
+console.log("hello " + myName);
