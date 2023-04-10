680ac81ddb83d1b320587ca49d76036260bd1630
Juahjoah <07juah21@gmail.com>
2023-04-10 17:20:42 +0900

    comment_views.py

--- Django/comment.py
+++ Django/comment.py
@@ -1,19 +1,33 @@
-# 게시글 리스트와 상세보기 페이지에 댓글의 개수를 표시
+# Create : 사용자로부터 댓글 데이터를 입력 받기 위한 CommentForm 작성
 
-# article/models.py
-class Free(models.Model):
-    comments = models.PositiveIntegerField(verbose_name='댓글수', null=True)
-
-# DB에 적용하기 위해서 migrations 적용
-# python manage.py makemigrations
-# python manage.py migrate
+# article/forms..py
+from django import forms
+from .models import Article, Comment
+#  ~
+class CommentForm(forms.ModelForm):
+    class Meta :
+        model = Comment
+        # fields = '__all__'
+        exclude = ('article',)
 
 #  article/views.py
-def detail(request, pk):
-    comment = Comment.objects.filter(post=pk).order_by('created')
-    comment_count = comment.exclude(deleted=True).count()
+from .forms import ArticleForm, CommentForm
 
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm()
     context = {
-        'comments': comment,
-        'comment_count': comment_count,
+        'article': article,
+        'comment_form' : comment_form,
     }
+    return render(request, 'articles/detail.html', context)
+
+def comments_create(request, pk):
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        # 바로 save하는게 아니라, DB저장하기 전에 잠깐 대기 시키기
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.save()
+    return redirect('articles:detail', article.pk)
