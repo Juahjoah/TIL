dd8f6c30ba05009da6fbb30b883ddd8cdd6ff1aa
Juahjoah <07juah21@gmail.com>
2023-04-10 17:17:54 +0900

    comment_detail.py

--- Django/comment.py
+++ Django/comment.py
@@ -1,1 +1,19 @@
 # 게시글 리스트와 상세보기 페이지에 댓글의 개수를 표시
+
+# article/models.py
+class Free(models.Model):
+    comments = models.PositiveIntegerField(verbose_name='댓글수', null=True)
+
+# DB에 적용하기 위해서 migrations 적용
+# python manage.py makemigrations
+# python manage.py migrate
+
+#  article/views.py
+def detail(request, pk):
+    comment = Comment.objects.filter(post=pk).order_by('created')
+    comment_count = comment.exclude(deleted=True).count()
+
+    context = {
+        'comments': comment,
+        'comment_count': comment_count,
+    }
