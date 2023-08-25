13eb08c87fb2b70690132a610c0067cf116b6940
Juahjoah <07juah21@gmail.com>
2023-08-25 13:41:56 +0900

    study: create homework folder

--- TS/problem/functionsExercise.js
+++ TS/problem/functionsExercise.js
@@ -4,57 +4,50 @@
 // Write a function called "twoFer" that accepts a person's name
 // It should return a string in the format "one for <name>, one for me"
 // If no name is provided, it should default to "you"
-
 // twoFer() => "One for you, one for me"
 // twoFer("Elton") => "One for Elton, one for me"
-
 // homework part 1
-function twoFer(person: string = "you"): string {
-  return `One for ${person}, one for me`;
+function twoFer(person) {
+    if (person === void 0) { person = "you"; }
+    return "One for ".concat(person, ", one for me");
 }
-
 console.log(twoFer());
 console.log(twoFer("Elvis"));
-
 // **********************************************
 // ******************* PART 2 *******************
 // **********************************************
 // Write a isLeapyear() function that accepts a year and returns true/false depending on if the year is a leap year
 // isLeapYear(2012) => true
 // isLeapYear(2013) => false
-
 // To determine whether a year is a leapyear, use this "formula":
 // A YEAR IS A LEAPYEAR IF
 // - year is a multiple of 4 AND not a multiple of 100
 // OR...
 // - year is a multiple of 400
 // hint - use modulo
-
-function isLeapyear(year: number): boolean {
-  if (year % 4 === 0 && year % 100 !== 0) {
-    return true;
-  } else if (year % 400 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+function isLeapyear(year) {
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    }
+    else if (year % 400 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-
-const isLeapYear2 = (yearTwo: number): boolean => {
-  if (yearTwo % 4 === 0 && yearTwo % 100 !== 0) {
-    return true;
-  } else if (yearTwo % 400 === 0) {
-    return true;
-  }
-  return false;
+var isLeapYear2 = function (yearTwo) {
+    if (yearTwo % 4 === 0 && yearTwo % 100 !== 0) {
+        return true;
+    }
+    else if (yearTwo % 400 === 0) {
+        return true;
+    }
+    return false;
 };
-
 // 간단한 방식으로 리팩토링하기
-const isLeapYear3 = (yearThree: number): boolean => {
-  return (
-    (yearThree % 4 === 0 && yearThree % 100 !== 0) || yearThree % 400 === 0
-  );
+var isLeapYear3 = function (yearThree) {
+    return ((yearThree % 4 === 0 && yearThree % 100 !== 0) || yearThree % 400 === 0);
 };
-
 console.log(isLeapYear3(2012));
 console.log(isLeapYear3(2013));
