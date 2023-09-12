72b524c55116742b141edcc8a98c1f838081baaf
Juahjoah <07juah21@gmail.com>
2023-09-13 02:33:37 +0900

    feat: import payphone

--- React/R3F/src/App.tsx
+++ React/R3F/src/App.tsx
@@ -1,15 +1,16 @@
 import { Canvas } from "@react-three/fiber";
-// import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
 import Payphone from "./Payphone";
 
-export default function App() {
+function App() {
   return (
-    // <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
-    <Canvas>
-      {/* <Environment preset="forest" /> */}
-      <Payphone />
-      {/* <ContactShadows position={[0, -0.8, 0]} color="#ffffff" /> */}
-      {/* <OrbitControls autoRotate /> */}
-    </Canvas>
+    <div style={{ width: "100%", height: "100vh" }}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Payphone />
+      </Canvas>
+    </div>
   );
 }
+
+export default App;
--- React/R3F/src/Payphone.jsx
+++ React/R3F/src/Payphone.jsx
@@ -1,7 +1,18 @@
-export default function Payphone(props) {
+import React, { useRef } from "react";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
+
+const Payphone = () => {
+  const gltf = useLoader(GLTFLoader, "/src/models/payphone.glb");
+  const group = useRef();
+
+  const initialPosition = [0, -3, -2];
+
   return (
-    <div>
-      <h1>Payphone</h1>
-    </div>
+    <group ref={group} position={initialPosition}>
+      <primitive object={gltf.scene} />
+    </group>
   );
-}
+};
+
+export default Payphone;
