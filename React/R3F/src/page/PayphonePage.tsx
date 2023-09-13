f464ee44804ba3248d2d653ba57b2e4cf38bb6e4
Juahjoah <07juah21@gmail.com>
2023-09-13 17:42:11 +0900

    feat: new payphone

--- React/R3F/src/page/PayphonePage.tsx
+++ React/R3F/src/page/PayphonePage.tsx
@@ -1,6 +1,7 @@
 import Payphone from "@/models/Payphone";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stage } from "@react-three/drei";
+import PayphoneGray from "./../component/Payphone/PayphoneGray";
 
 export default function PayphonePage() {
   return (
@@ -10,6 +11,7 @@ export default function PayphonePage() {
         <OrbitControls />
         <Stage environment="city" intensity={0.5} adjustCamera>
           <Payphone />
+          <PayphoneGray />
         </Stage>
       </Canvas>
     </div>
