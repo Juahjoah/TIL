a6db0bd5540f0870c7c04a7c387bae8c157b6178
Juahjoah <07juah21@gmail.com>
2025-03-30 14:17:09 +0900

    init : 한 입 자바스크립트 강의 수강 day2

---
+++ JavaScript/a bite size/src/index.js
@@ -0,0 +1,27 @@
+let name = 'joy';
+name = 'happyjoy'
+console.log(name);  // happyjoy
+
+
+// const myName = 'joy';
+// myName = 'happyjoy'
+
+// console.log(myName);  // Uncaught TypeError: Assignment to constant variable. at index.js:7:8
+
+const birthday = "12-21";
+console.log(birthday);  // 12-21
+
+const EMAIL_ADDRESS = "joy@gmail.com";
+const PROFILE_URL = "https://www.com";
+
+//typeof 
+let num = 10;
+
+console.log(typeof num); // "number"
+
+let name2 = "joy";
+console.log(typeof name2); // "string"
+
+let name3 = "joy";
+let intro = `제 이름은 ${name3}입니다.`; 
+console.log(intro); // "제 이름은 joy입니다."
